b1a72927fef208ed355445f3e5e64e1e222a8244
HwaDowan <126742782+HwaDowan@users.noreply.github.com>
2023-03-02 19:15:37 +0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+    
+    public static void LuasSegitiga() {
+
+    }
+
+    public static void LuasPersegi() {
+
+    }
+
+
+    public static void LuasTrapersium() {
+
+    }
+}
